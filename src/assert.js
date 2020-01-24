bac6972c657d9592ca2fc014faa952fc6f497e57
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2020-01-24 10:10:01 -0600

    Only exporting methods which need to be exported

--- src/assert.js
+++ src/assert.js
@@ -17,50 +17,50 @@ exports.deepStrictEqual = function(label, actual, expected) {
 
 	var expectedKind = KindOf(expected);
 
-	exports._isKind(label, actual, expectedKind);
+	isKind(label, actual, expectedKind);
 
 	switch (expectedKind) {
 
-		case 'date': return exports._deepStrictEqualDate(label, actual, expected);
-		case 'error': return exports._deepStrictEqualError(label, actual, expected);
-		case 'buffer': return exports._deepStrictEqualBuffer(label, actual, expected);
-		case 'array': return exports._deepStrictEqualArray(label, actual, expected);
-		case 'object': return exports._deepStrictEqualObject(label, actual, expected);
-		case 'arguments': return exports._deepStrictEqualArguments(label, actual, expected);
-		case 'regexp': return exports._deepStrictEqualRegexp(label, actual, expected);
-		case 'map': return exports._deepStrictEqualMap(label, actual, expected);
-		case 'set': return exports._deepStrictEqualSet(label, actual, expected);
+		case 'date': return deepStrictEqualDate(label, actual, expected);
+		case 'error': return deepStrictEqualError(label, actual, expected);
+		case 'buffer': return deepStrictEqualBuffer(label, actual, expected);
+		case 'array': return deepStrictEqualArray(label, actual, expected);
+		case 'object': return deepStrictEqualObject(label, actual, expected);
+		case 'arguments': return deepStrictEqualArguments(label, actual, expected);
+		case 'regexp': return deepStrictEqualRegexp(label, actual, expected);
+		case 'map': return deepStrictEqualMap(label, actual, expected);
+		case 'set': return deepStrictEqualSet(label, actual, expected);
 		default: return exports.strictEqual(label, actual, expected);
 	}
 };
 
-exports._deepStrictEqualDate = function(label, actual, expected) {
+function deepStrictEqualDate(label, actual, expected) {
 	Prove('SOO', arguments);
 	actual = actual.toString();
 	expected = expected.toString();
 	exports.strictEqual(label, actual, expected);
-};
+}
 
-exports._deepStrictEqualError = function(label, actual, expected) {
+function deepStrictEqualError(label, actual, expected) {
 	Prove('SEE', arguments);
 	actual = actual.toString();
 	expected = expected.toString();
 	exports.strictEqual(label, actual, expected);
-};
+}
 
-exports._deepStrictEqualBuffer = function(label, actual, expected) {
+function deepStrictEqualBuffer(label, actual, expected) {
 	Prove('S**', arguments);
 	Assert.ok((actual.equals(expected)), `Expected '${label}' to be '${expected}' but got '${actual}'.`);
-};
+}
 
-exports._deepStrictEqualRegexp = function(label, actual, expected) {
+function deepStrictEqualRegexp(label, actual, expected) {
 	Prove('S**', arguments);
 	actual = actual.toString();
 	expected = expected.toString();
 	exports.strictEqual(label, actual, expected);
-};
+}
 
-exports._deepStrictEqualArray = function(label, actual, expected) {
+function deepStrictEqualArray(label, actual, expected) {
 
 	Prove('SAA', arguments);
 
@@ -70,9 +70,9 @@ exports._deepStrictEqualArray = function(label, actual, expected) {
 		var actualVal = actual[i];
 		exports.deepStrictEqual(`${label}[${i}]`, actualVal, expectedVal);
 	});
-};
+}
 
-exports._deepStrictEqualMap = function(label, actual, expected) {
+function deepStrictEqualMap(label, actual, expected) {
 
 	Prove('S**', arguments);
 
@@ -86,9 +86,9 @@ exports._deepStrictEqualMap = function(label, actual, expected) {
 	actual.forEach(function(actualVal, key) {
 		Assert.ok((expected.has(key)), `Expected '${label}' not to have key '${key}' but it does.`);
 	});
-};
+}
 
-exports._deepStrictEqualSet = function(label, actual, expected) {
+function deepStrictEqualSet(label, actual, expected) {
 
 	Prove('S**', arguments);
 
@@ -103,9 +103,9 @@ exports._deepStrictEqualSet = function(label, actual, expected) {
 		var expectedVal = expectedIter.next().value;
 		exports.deepStrictEqual(`${label}[${i}]`, actualVal, expectedVal);
 	}
-};
+}
 
-exports._deepStrictEqualObject = function(label, actual, expected) {
+function deepStrictEqualObject(label, actual, expected) {
 
 	Prove('S**', arguments);
 
@@ -121,14 +121,14 @@ exports._deepStrictEqualObject = function(label, actual, expected) {
 	actualKeys.forEach(function(key) {
 		Assert.ok((expected.hasOwnProperty(key)), `Expected '${label}' not to have property '${key}' but it does.`);
 	});
-};
+}
 
-exports._deepStrictEqualArguments = function(label, actual, expected) {
+function deepStrictEqualArguments(label, actual, expected) {
 	Prove('S**', arguments);
 	actual = [].slice.call(actual, 0);
 	expected = [].slice.call(expected, 0);
-	exports._deepStrictEqualArray(label, actual, expected);
-};
+	deepStrictEqualArray(label, actual, expected);
+}
 
 exports.deepNarrowStrictEqual = function(label, actual, expected) {
 
@@ -136,24 +136,24 @@ exports.deepNarrowStrictEqual = function(label, actual, expected) {
 
 	var expectedKind = KindOf(expected);
 
-	exports._isKind(label, actual, expectedKind);
+	isKind(label, actual, expectedKind);
 
 	switch (expectedKind) {
 
-		case 'date': return exports._deepStrictEqualDate(label, actual, expected);
-		case 'error': return exports._deepStrictEqualError(label, actual, expected);
-		case 'buffer': return exports._deepStrictEqualBuffer(label, actual, expected);
-		case 'array': return exports._deepNarrowStrictEqualArray(label, actual, expected);
-		case 'object': return exports._deepNarrowStrictEqualObject(label, actual, expected);
-		case 'arguments': return exports._deepNarrowStrictEqualArguments(label, actual, expected);
-		case 'regexp': return exports._deepStrictEqualRegexp(label, actual, expected);
-		case 'map': return exports._deepNarrowStrictEqualMap(label, actual, expected);
-		case 'set': return exports._deepNarrowStrictEqualSet(label, actual, expected);
+		case 'date': return deepStrictEqualDate(label, actual, expected);
+		case 'error': return deepStrictEqualError(label, actual, expected);
+		case 'buffer': return deepStrictEqualBuffer(label, actual, expected);
+		case 'array': return deepNarrowStrictEqualArray(label, actual, expected);
+		case 'object': return deepNarrowStrictEqualObject(label, actual, expected);
+		case 'arguments': return deepNarrowStrictEqualArguments(label, actual, expected);
+		case 'regexp': return deepStrictEqualRegexp(label, actual, expected);
+		case 'map': return deepNarrowStrictEqualMap(label, actual, expected);
+		case 'set': return deepNarrowStrictEqualSet(label, actual, expected);
 		default: return exports.strictEqual(label, actual, expected);
 	}
 };
 
-exports._deepNarrowStrictEqualArray = function(label, actual, expected) {
+function deepNarrowStrictEqualArray(label, actual, expected) {
 
 	Prove('SAA', arguments);
 
@@ -161,9 +161,9 @@ exports._deepNarrowStrictEqualArray = function(label, actual, expected) {
 		var actualVal = actual[i];
 		exports.deepNarrowStrictEqual(`${label}[${i}]`, actualVal, expectedVal);
 	});
-};
+}
 
-exports._deepNarrowStrictEqualMap = function(label, actual, expected) {
+function deepNarrowStrictEqualMap(label, actual, expected) {
 
 	Prove('S**', arguments);
 
@@ -171,9 +171,9 @@ exports._deepNarrowStrictEqualMap = function(label, actual, expected) {
 		var actualVal = actual.get(key);
 		exports.deepNarrowStrictEqual(`${label}[${key}]`, actualVal, expectedVal);
 	});
-};
+}
 
-exports._deepNarrowStrictEqualSet = function(label, actual, expected) {
+function deepNarrowStrictEqualSet(label, actual, expected) {
 
 	Prove('S**', arguments);
 
@@ -186,9 +186,9 @@ exports._deepNarrowStrictEqualSet = function(label, actual, expected) {
 		var expectedVal = expectedIter.next().value;
 		exports.deepNarrowStrictEqual(`${label}[${i}]`, actualVal, expectedVal);
 	}
-};
+}
 
-exports._deepNarrowStrictEqualObject = function(label, actual, expected) {
+function deepNarrowStrictEqualObject(label, actual, expected) {
 
 	Prove('S**', arguments);
 
@@ -200,14 +200,14 @@ exports._deepNarrowStrictEqualObject = function(label, actual, expected) {
 		var expectedVal = expected[key];
 		exports.deepNarrowStrictEqual(`${label}[${key}]`, actualVal, expectedVal);
 	});
-};
+}
 
-exports._deepNarrowStrictEqualArguments = function(label, actual, expected) {
+function deepNarrowStrictEqualArguments(label, actual, expected) {
 	Prove('S**', arguments);
 	actual = [].slice.call(actual, 0);
 	expected = [].slice.call(expected, 0);
-	exports._deepNarrowStrictEqualArray(label, actual, expected);
-};
+	deepNarrowStrictEqualArray(label, actual, expected);
+}
 
 exports.deepKind = function(label, actual, expected) {
 
@@ -215,22 +215,22 @@ exports.deepKind = function(label, actual, expected) {
 
 	var expectedIsKind = (typeof expected === 'string');
 
-	if (expectedIsKind) return exports._isKind(label, actual, expected);
+	if (expectedIsKind) return isKind(label, actual, expected);
 
 	var expectedKind = KindOf(expected);
 
-	exports._isKind(label, actual, expectedKind);
+	isKind(label, actual, expectedKind);
 
 	switch (expectedKind) {
-		case 'array': return exports._deepKindArray(label, actual, expected);
-		case 'object': return exports._deepKindObject(label, actual, expected);
-		case 'arguments': return exports._deepKindArguments(label, actual, expected);
-		case 'map': return exports._deepKindMap(label, actual, expected);
+		case 'array': return deepKindArray(label, actual, expected);
+		case 'object': return deepKindObject(label, actual, expected);
+		case 'arguments': return deepKindArguments(label, actual, expected);
+		case 'map': return deepKindMap(label, actual, expected);
 		default: Assert.fail(`Invalid kind '${expectedKind}' for '${label}'.`);
 	}
 };
 
-exports._deepKindArray = function(label, actual, expected) {
+function deepKindArray(label, actual, expected) {
 
 	Prove('SAA', arguments);
 
@@ -240,9 +240,9 @@ exports._deepKindArray = function(label, actual, expected) {
 		var actualVal = actual[i];
 		exports.deepKind(`${label}[${i}]`, actualVal, expectedVal);
 	});
-};
+}
 
-exports._deepKindMap = function(label, actual, expected) {
+function deepKindMap(label, actual, expected) {
 
 	Prove('S**', arguments);
 
@@ -256,9 +256,9 @@ exports._deepKindMap = function(label, actual, expected) {
 	actual.forEach(function(actualVal, key) {
 		Assert.ok((expected.has(key)), `Expected '${label}' not to have key '${key}' but it does.`);
 	});
-};
+}
 
-exports._deepKindObject = function(label, actual, expected) {
+function deepKindObject(label, actual, expected) {
 
 	Prove('S**', arguments);
 
@@ -274,105 +274,105 @@ exports._deepKindObject = function(label, actual, expected) {
 	actualKeys.forEach(function(key) {
 		Assert.ok((expected.hasOwnProperty(key)), `Expected '${label}' not to have property '${key}' but it does.`);
 	});
-};
+}
 
-exports._deepKindArguments = function(label, actual, expected) {
+function deepKindArguments(label, actual, expected) {
 	Prove('S**', arguments);
 	actual = [].slice.call(actual, 0);
 	expected = [].slice.call(expected, 0);
-	exports._deepKindArray(label, actual, expected);
-};
+	deepKindArray(label, actual, expected);
+}
 
 exports.isArray = function(label, actualVal) {
 	Prove('S*', arguments);
-	exports._isKind(label, actualVal, 'array');
+	isKind(label, actualVal, 'array');
 };
 
 exports.isBoolean = function(label, actualVal) {
 	Prove('S*', arguments);
-	exports._isKind(label, actualVal, 'boolean');
+	isKind(label, actualVal, 'boolean');
 };
 
 exports.isBuffer = function(label, actualVal) {
 	Prove('S*', arguments);
-	exports._isKind(label, actualVal, 'buffer');
+	isKind(label, actualVal, 'buffer');
 };
 
 exports.isDate = function(label, actualVal) {
 	Prove('S*', arguments);
-	exports._isKind(label, actualVal, 'date');
+	isKind(label, actualVal, 'date');
 };
 
 exports.isError = function(label, actualVal) {
 	Prove('S*', arguments);
-	exports._isKind(label, actualVal, 'error');
+	isKind(label, actualVal, 'error');
 };
 
 exports.isFunction = function(label, actualVal) {
 	Prove('S*', arguments);
-	exports._isKind(label, actualVal, 'function');
+	isKind(label, actualVal, 'function');
 };
 
 exports.isMap = function(label, actualVal) {
 	Prove('S*', arguments);
-	exports._isKind(label, actualVal, 'map');
+	isKind(label, actualVal, 'map');
 };
 
 exports.isNumber = function(label, actualVal) {
 	Prove('S*', arguments);
-	exports._isKind(label, actualVal, 'number');
+	isKind(label, actualVal, 'number');
 };
 
 exports.isNull = function(label, actualVal) {
 	Prove('S*', arguments);
-	exports._isKind(label, actualVal, 'null');
+	isKind(label, actualVal, 'null');
 };
 
 exports.isObject = function(label, actualVal) {
 	Prove('S*', arguments);
-	exports._isKind(label, actualVal, 'object');
+	isKind(label, actualVal, 'object');
 };
 
 exports.isPromise = function(label, actualVal) {
 	Prove('S*', arguments);
-	exports._isKind(label, actualVal, 'promise');
+	isKind(label, actualVal, 'promise');
 };
 
 exports.isRegexp = function(label, actualVal) {
 	Prove('S*', arguments);
-	exports._isKind(label, actualVal, 'regexp');
+	isKind(label, actualVal, 'regexp');
 };
 
 exports.isSet = function(label, actualVal) {
 	Prove('S*', arguments);
-	exports._isKind(label, actualVal, 'set');
+	isKind(label, actualVal, 'set');
 };
 
 exports.isString = function(label, actualVal) {
 	Prove('S*', arguments);
-	exports._isKind(label, actualVal, 'string');
+	isKind(label, actualVal, 'string');
 };
 
 exports.isSymbol = function(label, actualVal) {
 	Prove('S*', arguments);
-	exports._isKind(label, actualVal, 'symbol');
+	isKind(label, actualVal, 'symbol');
 };
 
 exports.isUint8Array = function(label, actualVal) {
 	Prove('S*', arguments);
-	exports._isKind(label, actualVal, 'uint8array');
+	isKind(label, actualVal, 'uint8array');
 };
 
 exports.isUndefined = function(label, actualVal) {
 	Prove('S*', arguments);
-	exports._isKind(label, actualVal, 'undefined');
+	isKind(label, actualVal, 'undefined');
 };
 
-exports._isKind = function(label, actualVal, expectedKind) {
+function isKind(label, actualVal, expectedKind) {
 	Prove('S**', arguments);
 	var actualKind = KindOf(actualVal);
 	Assert.ok((actualKind === expectedKind), `Expected kind of '${label}' to be '${expectedKind}' but got '${actualKind}'.`);
-};
+}
 
 exports.isEmpty = function(label, val) {
 	Prove('S*', arguments);
